ff5a847506bc4254f80fb07323a28135b4562ade
MUQDZPCQXJCJNKD\Administrator <shenshanbr@163.com>
2017-10-22 21:09:02 +0800

    job4

---
+++ Job4MP4/app/src/main/java/com/manu/job5mp4/MainActivity.java
@@ -0,0 +1,349 @@
+package com.manu.job5mp4;
+
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.media.MediaCodec;
+import android.media.MediaExtractor;
+import android.media.MediaFormat;
+import android.media.MediaMuxer;
+import android.os.Build;
+import android.os.Environment;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+
+/**
+ * http://blog.csdn.net/zhi184816/article/details/52514138
+ */
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+            "android.permission.READ_EXTERNAL_STORAGE",
+            "android.permission.WRITE_EXTERNAL_STORAGE",
+            "android.permission.RECORD_AUDIO"};
+
+
+    public  void verifyStoragePermissions(Activity activity) {
+
+        try {
+            //检测是否有写的权限
+            int permission = ActivityCompat.checkSelfPermission(activity,
+                    "android.permission.WRITE_EXTERNAL_STORAGE");
+            int permission2 = ActivityCompat.checkSelfPermission(activity,
+                    "android.permission.RECORD_AUDIO");
+            if (permission != PackageManager.PERMISSION_GRANTED || permission2 != PackageManager.PERMISSION_GRANTED) {
+                // 没有写的权限，去申请写的权限，会弹出对话框
+                ActivityCompat.requestPermissions(activity, PERMISSIONS_STORAGE,REQUEST_EXTERNAL_STORAGE);
+            }else{
+                Toast.makeText(this, "Start Testing !", Toast.LENGTH_SHORT).show();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults)
+    {
+
+        if (requestCode == REQUEST_EXTERNAL_STORAGE)
+        {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED)
+            {
+                Toast.makeText(this, "Start Testing !", Toast.LENGTH_SHORT).show();
+            } else
+            {
+                // Permission Denied
+                Toast.makeText(MainActivity.this, "Permission Denied", Toast.LENGTH_SHORT).show();
+            }
+            return;
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //请求权限
+        verifyStoragePermissions(MainActivity.this);
+        initView();
+    }
+    private Button mediaextractorBt;
+    private Button muxermediaBt;
+    private Button muxeraudioBt;
+    private MediaExtractor mediaExtractor;
+    private static String SDCARD_PATH = Environment.getExternalStorageDirectory()+"";
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
+    private void initView() {
+        mediaextractorBt = (Button)findViewById(R.id.mediaextractor_bt);
+        mediaextractorBt.setOnClickListener(this);
+        muxermediaBt = (Button)findViewById(R.id.muxermedia_bt);
+        muxermediaBt.setOnClickListener(this);
+        muxeraudioBt = (Button)findViewById(R.id.muxeraudio_bt);
+        muxeraudioBt.setOnClickListener(this);
+        mediaExtractor = new MediaExtractor();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR2)
+    @Override
+    public void onClick(View view) {
+        if(view.getId() == R.id.mediaextractor_bt){
+//            new Thread(){
+//                @Override
+//                public void run() {
+//                    super.run();
+//                    exactorMedia();
+//                }
+//            };
+            exactorMedia();
+
+        }else if(view.getId() == R.id.muxermedia_bt){
+            muxerMedia();
+        }else if(view.getId() == R.id.muxeraudio_bt){
+            muxerAudio();
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
+    private void exactorMedia() {
+        FileOutputStream videoOutputStream = null;
+        FileOutputStream audioOutputStream = null;
+        try {
+            //分离的视频文件
+            File videoFile = new File(SDCARD_PATH, "output_video.mp4");
+            //分离的音频文件
+//            File audioFile = new File(SDCARD_PATH, "output_audio");
+            File audioFile = new File(SDCARD_PATH, "output_audio.mp3");
+            videoOutputStream = new FileOutputStream(videoFile);
+            audioOutputStream = new FileOutputStream(audioFile);
+            //源文件
+            File totalFile = new File(SDCARD_PATH ,"test.mp4");
+            mediaExtractor.setDataSource(SDCARD_PATH + "/test.mp4");
+            //信道总数
+            int trackCount = mediaExtractor.getTrackCount();
+            int audioTrackIndex = -1;
+            int videoTrackIndex = -1;
+            for (int i = 0; i < trackCount; i++) {
+                MediaFormat trackFormat = mediaExtractor.getTrackFormat(i);
+                String mineType = trackFormat.getString(MediaFormat.KEY_MIME);
+                //视频信道
+                if (mineType.startsWith("video/")) {
+                    videoTrackIndex = i;
+                }
+                //音频信道
+                if (mineType.startsWith("audio/")) {
+                    audioTrackIndex = i;
+                }
+            }
+
+            ByteBuffer byteBuffer = ByteBuffer.allocate(500 * 1024);
+            //切换到视频信道
+            mediaExtractor.selectTrack(videoTrackIndex);
+            while (true) {
+                int readSampleCount = mediaExtractor.readSampleData(byteBuffer, 0);
+                if (readSampleCount < 0) {
+                    break;
+                }
+                //保存视频信道信息
+                byte[] buffer = new byte[readSampleCount];
+                byteBuffer.get(buffer);
+                videoOutputStream.write(buffer);
+                byteBuffer.clear();
+                mediaExtractor.advance();
+            }
+            //切换到音频信道
+            mediaExtractor.selectTrack(audioTrackIndex);
+            while (true) {
+                int readSampleCount = mediaExtractor.readSampleData(byteBuffer, 0);
+                if (readSampleCount < 0) {
+                    break;
+                }
+                //保存音频信息
+                byte[] buffer = new byte[readSampleCount];
+                byteBuffer.get(buffer);
+                audioOutputStream.write(buffer);
+                byteBuffer.clear();
+                mediaExtractor.advance();
+            }
+
+            Log.d("aaa","1"+getFileSize(videoFile)+"----2--"+getFileSize(audioFile)+"---3-"+getFileSize(totalFile));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            mediaExtractor.release();
+            try {
+                videoOutputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * 获取指定文件大小
+     * @param file
+     * @return
+     * @throws Exception 　　
+     */
+    public static long getFileSize(File file) throws Exception {
+        long size = 0;
+        if (file.exists()) {
+            FileInputStream fis = null;
+            fis = new FileInputStream(file);
+            size = fis.available();
+        } else {
+            file.createNewFile();
+            Log.e("获取文件大小", "文件不存在!");
+        }
+        return size;
+    }
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    private MediaMuxer mediaMuxer;
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR2)
+    private void muxerMedia() {
+        mediaExtractor = new MediaExtractor();
+        int videoIndex = -1;
+        try {
+            mediaExtractor.setDataSource(SDCARD_PATH + "/test.mp4");
+            int trackCount = mediaExtractor.getTrackCount();
+            for (int i = 0; i < trackCount; i++) {
+                MediaFormat trackFormat = mediaExtractor.getTrackFormat(i);
+                String mimeType = trackFormat.getString(MediaFormat.KEY_MIME);
+                // 取出视频的信号
+                if (mimeType.startsWith("video/")) {
+                    videoIndex = i;
+                }
+            }
+            //切换道视频信号的信道
+            mediaExtractor.selectTrack(videoIndex);
+            MediaFormat trackFormat = mediaExtractor.getTrackFormat(videoIndex);
+            mediaMuxer = new MediaMuxer(SDCARD_PATH + "/output_video.mp4", MediaMuxer.OutputFormat.MUXER_OUTPUT_MPEG_4);
+            //追踪此信道
+            int trackIndex = mediaMuxer.addTrack(trackFormat);
+            ByteBuffer byteBuffer = ByteBuffer.allocate(1024 * 500);
+            MediaCodec.BufferInfo bufferInfo = new MediaCodec.BufferInfo();
+            mediaMuxer.start();
+            long videoSampleTime;
+            //获取每帧的之间的时间
+            {
+                mediaExtractor.readSampleData(byteBuffer, 0);
+                //skip first I frame
+                if (mediaExtractor.getSampleFlags() == MediaExtractor.SAMPLE_FLAG_SYNC)
+                    mediaExtractor.advance();
+                mediaExtractor.readSampleData(byteBuffer, 0);
+                long firstVideoPTS = mediaExtractor.getSampleTime();
+                mediaExtractor.advance();
+                mediaExtractor.readSampleData(byteBuffer, 0);
+                long SecondVideoPTS = mediaExtractor.getSampleTime();
+                videoSampleTime = Math.abs(SecondVideoPTS - firstVideoPTS);
+                Log.d("fuck", "videoSampleTime is " + videoSampleTime);
+            }
+            //重新切换此信道，不然上面跳过了3帧,造成前面的帧数模糊
+            mediaExtractor.unselectTrack(videoIndex);
+            mediaExtractor.selectTrack(videoIndex);
+            while (true) {
+                //读取帧之间的数据
+                int readSampleSize = mediaExtractor.readSampleData(byteBuffer, 0);
+                if (readSampleSize < 0) {
+                    break;
+                }
+                mediaExtractor.advance();
+                bufferInfo.size = readSampleSize;
+                bufferInfo.offset = 0;
+
+//                bufferInfo.flags = mediaExtractor.getSampleFlags();//TODO 修改一下试试
+                bufferInfo.flags = MediaExtractor.SAMPLE_FLAG_SYNC;
+                bufferInfo.presentationTimeUs += videoSampleTime;
+                //写入帧的数据
+                mediaMuxer.writeSampleData(trackIndex, byteBuffer, bufferInfo);
+            }
+            //release
+            mediaMuxer.stop();
+            mediaExtractor.release();
+            mediaMuxer.release();
+
+            Log.e("TAG", "finish");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR2)
+    private void muxerAudio() {
+        mediaExtractor = new MediaExtractor();
+        int audioIndex = -1;
+        try {
+            mediaExtractor.setDataSource(SDCARD_PATH + "/test.mp4");
+            int trackCount = mediaExtractor.getTrackCount();
+            for (int i = 0; i < trackCount; i++) {
+                MediaFormat trackFormat = mediaExtractor.getTrackFormat(i);
+                if (trackFormat.getString(MediaFormat.KEY_MIME).startsWith("audio/")) {
+                    audioIndex = i;
+                }
+            }
+            mediaExtractor.selectTrack(audioIndex);
+            MediaFormat trackFormat = mediaExtractor.getTrackFormat(audioIndex);
+            mediaMuxer = new MediaMuxer(SDCARD_PATH + "/output_audios", MediaMuxer.OutputFormat.MUXER_OUTPUT_MPEG_4);
+            int writeAudioIndex = mediaMuxer.addTrack(trackFormat);
+            mediaMuxer.start();
+            ByteBuffer byteBuffer = ByteBuffer.allocate(500 * 1024);
+            MediaCodec.BufferInfo bufferInfo = new MediaCodec.BufferInfo();
+
+            long stampTime = 0;
+            //获取帧之间的间隔时间
+            {
+                mediaExtractor.readSampleData(byteBuffer, 0);
+                if (mediaExtractor.getSampleFlags() == MediaExtractor.SAMPLE_FLAG_SYNC) {
+                    mediaExtractor.advance();
+                }
+                mediaExtractor.readSampleData(byteBuffer, 0);
+                long secondTime = mediaExtractor.getSampleTime();
+                mediaExtractor.advance();
+                mediaExtractor.readSampleData(byteBuffer, 0);
+                long thirdTime = mediaExtractor.getSampleTime();
+                stampTime = Math.abs(thirdTime - secondTime);
+                Log.e("fuck", stampTime + "");
+            }
+
+            mediaExtractor.unselectTrack(audioIndex);
+            mediaExtractor.selectTrack(audioIndex);
+            while (true) {
+                int readSampleSize = mediaExtractor.readSampleData(byteBuffer, 0);
+                if (readSampleSize < 0) {
+                    break;
+                }
+                mediaExtractor.advance();
+
+                bufferInfo.size = readSampleSize;
+                //                bufferInfo.flags = mediaExtractor.getSampleFlags();//TODO 修改一下试试
+                bufferInfo.flags = MediaExtractor.SAMPLE_FLAG_SYNC;
+                bufferInfo.offset = 0;
+                bufferInfo.presentationTimeUs += stampTime;
+
+                mediaMuxer.writeSampleData(writeAudioIndex, byteBuffer, bufferInfo);
+            }
+            mediaMuxer.stop();
+            mediaMuxer.release();
+            mediaExtractor.release();
+            Log.e("fuck", "finish");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+}
