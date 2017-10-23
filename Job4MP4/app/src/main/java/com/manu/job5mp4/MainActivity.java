0add83ef4546d9d01ba6ed8cbb33d848f2f37264
MUQDZPCQXJCJNKD\Administrator <shenshanbr@163.com>
2017-10-23 23:00:16 +0800

    合成视频

--- Job4MP4/app/src/main/java/com/manu/job5mp4/MainActivity.java
+++ Job4MP4/app/src/main/java/com/manu/job5mp4/MainActivity.java
@@ -84,6 +84,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private Button mediaextractorBt;
     private Button muxermediaBt;
     private Button muxeraudioBt;
+    private Button combineBt;
     private MediaExtractor mediaExtractor;
     private static String SDCARD_PATH = Environment.getExternalStorageDirectory()+"";
     @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
@@ -94,6 +95,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         muxermediaBt.setOnClickListener(this);
         muxeraudioBt = (Button)findViewById(R.id.muxeraudio_bt);
         muxeraudioBt.setOnClickListener(this);
+        combineBt = (Button)findViewById(R.id.combine_bt);
+        combineBt.setOnClickListener(this);
+
         mediaExtractor = new MediaExtractor();
     }
 
@@ -114,6 +118,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             muxerMedia();
         }else if(view.getId() == R.id.muxeraudio_bt){
             muxerAudio();
+        }else if(view.getId() == R.id.combine_bt){
+            combineVideo();
         }
     }
 
@@ -346,4 +352,99 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
     ///////////////////////////////////////////////////////////////////////////////////////////////
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR2)
+    private void combineVideo() {
+        try {
+            MediaExtractor videoExtractor = new MediaExtractor();
+            videoExtractor.setDataSource(SDCARD_PATH + "/output_video");
+            MediaFormat videoFormat = null;
+            int videoTrackIndex = -1;
+            int videoTrackCount = videoExtractor.getTrackCount();
+            for (int i = 0; i < videoTrackCount; i++) {
+                videoFormat = videoExtractor.getTrackFormat(i);
+                String mimeType = videoFormat.getString(MediaFormat.KEY_MIME);
+                if (mimeType.startsWith("video/")) {
+                    videoTrackIndex = i;
+                    break;
+                }
+            }
+
+            MediaExtractor audioExtractor = new MediaExtractor();
+            audioExtractor.setDataSource(SDCARD_PATH + "/output_audio");
+            MediaFormat audioFormat = null;
+            int audioTrackIndex = -1;
+            int audioTrackCount = audioExtractor.getTrackCount();
+            for (int i = 0; i < audioTrackCount; i++) {
+                audioFormat = audioExtractor.getTrackFormat(i);
+                String mimeType = audioFormat.getString(MediaFormat.KEY_MIME);
+                if (mimeType.startsWith("audio/")) {
+                    audioTrackIndex = i;
+                    break;
+                }
+            }
+
+            videoExtractor.selectTrack(videoTrackIndex);
+            audioExtractor.selectTrack(audioTrackIndex);
+
+            MediaCodec.BufferInfo videoBufferInfo = new MediaCodec.BufferInfo();
+            MediaCodec.BufferInfo audioBufferInfo = new MediaCodec.BufferInfo();
+
+            MediaMuxer mediaMuxer = new MediaMuxer(SDCARD_PATH + "/output", MediaMuxer.OutputFormat.MUXER_OUTPUT_MPEG_4);
+            int writeVideoTrackIndex = mediaMuxer.addTrack(videoFormat);
+            int writeAudioTrackIndex = mediaMuxer.addTrack(audioFormat);
+            mediaMuxer.start();
+
+            ByteBuffer byteBuffer = ByteBuffer.allocate(500 * 1024);
+            long sampleTime = 0;
+            {
+                videoExtractor.readSampleData(byteBuffer, 0);
+                if (videoExtractor.getSampleFlags() == MediaExtractor.SAMPLE_FLAG_SYNC) {
+                    videoExtractor.advance();
+                }
+                videoExtractor.readSampleData(byteBuffer, 0);
+                long secondTime = videoExtractor.getSampleTime();
+                videoExtractor.advance();
+                long thirdTime = videoExtractor.getSampleTime();
+                sampleTime = Math.abs(thirdTime - secondTime);
+            }
+            videoExtractor.unselectTrack(videoTrackIndex);
+            videoExtractor.selectTrack(videoTrackIndex);
+
+            while (true) {
+                int readVideoSampleSize = videoExtractor.readSampleData(byteBuffer, 0);
+                if (readVideoSampleSize < 0) {
+                    break;
+                }
+                videoBufferInfo.size = readVideoSampleSize;
+                videoBufferInfo.presentationTimeUs += sampleTime;
+                videoBufferInfo.offset = 0;
+//                videoBufferInfo.flags = videoExtractor.getSampleFlags();//TODO
+                videoBufferInfo.flags = MediaExtractor.SAMPLE_FLAG_SYNC;
+                mediaMuxer.writeSampleData(writeVideoTrackIndex, byteBuffer, videoBufferInfo);
+                videoExtractor.advance();
+            }
+
+            while (true) {
+                int readAudioSampleSize = audioExtractor.readSampleData(byteBuffer, 0);
+                if (readAudioSampleSize < 0) {
+                    break;
+                }
+
+                audioBufferInfo.size = readAudioSampleSize;
+                audioBufferInfo.presentationTimeUs += sampleTime;
+                audioBufferInfo.offset = 0;
+//                audioBufferInfo.flags = videoExtractor.getSampleFlags();//TODO
+                videoBufferInfo.flags = MediaExtractor.SAMPLE_FLAG_SYNC;
+                mediaMuxer.writeSampleData(writeAudioTrackIndex, byteBuffer, audioBufferInfo);
+                audioExtractor.advance();
+            }
+
+            mediaMuxer.stop();
+            mediaMuxer.release();
+            videoExtractor.release();
+            audioExtractor.release();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
